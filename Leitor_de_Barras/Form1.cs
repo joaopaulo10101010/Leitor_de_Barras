9386156346aa5ddabe17985b995a9b36accb2808
joaopaulo10101010 <joao_paulo_oandrade@hotmail.com>
2025-03-26 02:16:47 -0300

    Subindo o projeto para o repositorio

---
+++ Leitor_de_Barras/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Leitor_de_Barras
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Leitor leitor = new Leitor();
+
+            saidanumero.Text = leitor.LerBarra(caminho.Text);
+            
+            
+        }
+    }
+}
---
+++ Leitor_de_Barras/ReadControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace Leitor_de_Barras
+{
+    internal class ReadControl
+    {
+        public void LerBarra(TextBox a)
+        {
+            a.Text = "Funcionou";
+        }
+    }
+
+    public class Leitor 
+    {
+
+
+        public string LerBarra(string caminho_imagem)
+        {
+            // Definindo que o Bitmap(imagem) possui o endereço a função recebe
+            Bitmap imagem = new Bitmap(caminho_imagem);
+            // Definindo o Leitor de codigo de Barras
+            BarcodeReader leitor = new BarcodeReader();
+
+            // Guardando na variavel resultado, os dados lidos pelo leitor, com o seu metodo Decode
+            var resultado = leitor.Decode(imagem);
+
+            return resultado.Text;
+        }     
+    }
+}
